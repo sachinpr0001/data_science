724ebcf8033e5fbceec5c743ea06f8187bc91458
sachinpr0001 <sachinpr0001@gmail.com>
2021-09-05 15:16:46 +0530

    .

--- 05-data_acquisition/chuck_norris_jokes_assignment/jokes.py
+++ 05-data_acquisition/chuck_norris_jokes_assignment/jokes.py
@@ -1,36 +1,25 @@
-import csv
+import pandas as pd
+import numpy as np
 import requests
 import json
-import pandas as pd
 file = open('ID.csv')
 csvreader = csv.reader(file)
 rows = []
+
 for row in csvreader:
         rows.append(row)
 rows = rows[1:]
 jokes = []
-
+id_array = []
 for list_index in rows:
     id = list_index[0]
+    id_array.append(id)
     url = "http://api.icndb.com/jokes/{}".format(id)
     response = requests.get(url)
     joke_dict = json.loads(response.content.decode('utf-8'))
     joke = joke_dict["value"]["joke"]
     print(joke)
     jokes.append(joke)
-# print(jokes)
-
-csv = pd.DataFrame(jokes)[['ID','joke']]
-csv.to_csv("submission.csv",index=False)
-
-# df=pd.DataFrame(jokes, columns=['ID', 'Jokes']) 
-# print(df.head())
-
-
-
-
-# with open('output.csv','w') as result_file:
-#     wr = csv.writer(result_file)
-#     wr.writerows([jokes])
-
-print("csv created successfully")
+combined = np.vstack((id_array, jokes)).T
+df=pd.DataFrame(combined, columns=['ID', 'Jokes']) 
+df.to_csv("submission.csv", index=False)
